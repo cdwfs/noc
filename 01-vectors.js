c7d6c146400fc4b078f801305d8c10e2251d339c
Cort <github@dangerware.org>
2015-01-24 22:14:36 -0800

    First commit

---
+++ 01-vectors.js
@@ -0,0 +1,50 @@
+/* jshint browser: true
+   , strict: true
+   , bitwise: true
+   , eqeqeq: true
+   , forin: true
+   , freeze: true
+   , latedef: true
+   , newcap: true
+   , noarg: true
+   , undef: true
+   , plusplus: true
+*/
+
+/* global Two */
+
+
+var canvasElem = document.getElementById('twobox-canvas');
+var two = new Two({
+	type: Two.Types.webgl,
+	width: canvasElem.clientWidth,
+	height: canvasElem.clientHeight,
+    fullscreen: false,
+    autostart: true,
+	overdraw: true
+}).appendTo(canvasElem);
+
+var darkener = two.makeRectangle(two.width/2, two.height/2,
+								 two.width, two.height);
+darkener.fill = "black";
+darkener.opacity = 0.1;
+
+var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
+var mouse = new Two.Vector();
+rect.stroke = "green";
+rect.linewidth = 5;
+rect.fill = "orange";
+document.addEventListener( 'mousemove', function(e) {
+	"use strict";
+	mouse.x = e.clientX;
+	mouse.y = e.clientY;
+}, false );
+
+two.bind('update', function() {
+	"use strict";
+	two.width  = canvasElem.clientWidth;
+	two.height = canvasElem.clientHeight;
+	//rect.translation.set(two.width/2, two.height/2);
+	rect.translation.set(mouse.x, mouse.y);
+    rect.rotation += 0.005;
+});
