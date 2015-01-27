bfdfe657c0fca3546fbb9583b94bbd589d0ab922
Cort <github@dangerware.org>
2015-01-26 23:07:07 -0800

    walker

--- 01-vectors.js
+++ 01-vectors.js
@@ -30,27 +30,52 @@ var two = new Two({
 	overdraw: true
 }).appendTo(canvasElem);
 
-var darkener = two.makeRectangle(two.width/2, two.height/2,
-								 two.width, two.height);
-darkener.fill = "black";
-darkener.opacity = 0.1;
-
-var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
 var mouse = new Two.Vector();
-rect.stroke = "green";
-rect.linewidth = 5;
-rect.fill = "orange";
 document.addEventListener( 'mousemove', function(e) {
 	"use strict";
 	mouse.x = e.clientX;
 	mouse.y = e.clientY;
 }, false );
 
+if (two.overdraw) {
+	// Draw a full-screen quad to fade the existing framebuffer over
+	// time. Must be the first object in the scene!
+	var darkener = two.makeRectangle(two.width/2, two.height/2,
+									 two.width,   two.height);
+	darkener.fill = "black";
+	darkener.opacity = 0.001; // tune me to adjust the fade speed
+}
+
+//var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
+//rect.stroke = "green";
+//rect.linewidth = 5;
+//rect.fill = "orange";
+
+function newWalker() {
+	"use strict";
+	var m_rect = two.makeRectangle(two.width/2, two.height/2, 5,5);
+	m_rect.linewidth = 0.01;
+	m_rect.fill = "yellow";
+	return {
+		step: function() {
+			var oldTrans = m_rect.translation.clone();
+			oldTrans.x += randomInt(-1, 2);
+			oldTrans.y += randomInt(-1, 2);
+			m_rect.translation.copy(oldTrans);
+		},
+	};
+}
+var walker = newWalker();
+
 two.bind('update', function() {
 	"use strict";
 	two.width  = canvasElem.clientWidth;
 	two.height = canvasElem.clientHeight;
+
+	walker.step();
+	
 	//rect.translation.set(two.width/2, two.height/2);
-	rect.translation.set(mouse.x, mouse.y);
-    rect.rotation += 0.005;
+
+	//rect.translation.set(mouse.x, mouse.y);
+    //rect.rotation += 0.005;
 });
