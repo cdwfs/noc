030364c79eeb7ca7c24cee01c6b6f819b84addbb
Cort Stratton <cort_stratton@naughtydog.com>
2015-01-27 20:14:05 -0800

    Processing.js

--- 01-vectors.js
+++ 01-vectors.js
@@ -11,7 +11,9 @@
    , plusplus: true
 */
 
-/* global Two */
+/* global Processing
+   , PerlinSimplex
+*/
 
 // Returns a random integer from min (inclusive) to max (exclusive).
 function randomInt(min, max) {
@@ -44,48 +46,19 @@ function randomGaussian(mu, sigma) {
 	return mu + sigma*randomNormal();
 }
 
-
-var canvasElem = document.getElementById('twobox-canvas');
-var two = new Two({
-	type: Two.Types.webgl,
-	width: canvasElem.clientWidth,
-	height: canvasElem.clientHeight,
-    fullscreen: false,
-    autostart: true,
-	overdraw: true
-}).appendTo(canvasElem);
-
-var mouse = new Two.Vector();
-document.addEventListener( 'mousemove', function(e) {
-	"use strict";
-	mouse.x = e.clientX;
-	mouse.y = e.clientY;
-}, false );
-
-if (two.overdraw) {
-	// Draw a full-screen quad to fade the existing framebuffer over
-	// time. Must be the first object in the scene!
-	//
-	// TODO: for lower values, the old contents never seem to
-	// disappear completely...
-	var darkener = two.makeRectangle(two.width/2, two.height/2,
-									 two.width,   two.height);
-	darkener.fill = "black";
-	darkener.opacity = 0.5; // tune me to adjust the fade speed
-}
-
-//var rect = two.makeRectangle(two.width / 2, two.height / 2, 50 ,50);
-//rect.stroke = "green";
-//rect.linewidth = 5;
-//rect.fill = "orange";
+var pjs = null;
 
 function newWalker() {
 	"use strict";
-	var m_line = two.makeLine(two.width/2, two.height/2,
-							  two.width/2, two.height/2);
-	m_line.linewidth = 3;
-	m_line.stroke = "yellow";
+	var m_x0 = 0, m_y0 = 0, m_x1 = 0, m_y1 = 0;
+	var m_tx = 0, m_ty = 10000;
+	PerlinSimplex.noiseDetail(4,0);
 	return {
+		draw: function() {
+			pjs.stroke(255, 255, 0);
+			pjs.strokeWeight(3);
+			pjs.line(m_x0, m_y0, m_x1, m_y1);
+		},
 		step: function() {
 			var monteCarlo = function() {
 				while(true) {
@@ -97,25 +70,57 @@ function newWalker() {
 				}
 			};
 			var stepSize = 30 * (1-monteCarlo());
-			m_line.vertices[0].copy(m_line.vertices[1]);
-			m_line.vertices[1].set(
-				m_line.vertices[1].x + stepSize*(Math.random()*2-1),
-				m_line.vertices[1].y + stepSize*(Math.random()*2-1)
-			);
+			m_x0 = m_x1;
+			m_y0 = m_y1;
+			m_x1 = pjs.width  * PerlinSimplex.noise(m_tx, 0);
+			m_y1 = pjs.height * PerlinSimplex.noise(m_ty, 0);
+			m_tx += 0.01;
+			m_ty += 0.01;
 		},
 	};
 }
 var walker = newWalker();
 
-two.bind('update', function() {
+function sketchProc(pjs) {
 	"use strict";
-	two.width  = canvasElem.clientWidth;
-	two.height = canvasElem.clientHeight;
+	// Override draw function, by default it will be called 60 times per second
+	pjs.size(640, 480);
+	pjs.draw = function() {
+		// determine center and max clock arm length
+		var centerX = pjs.width / 2, centerY = pjs.height / 2;
+		var maxArmLength = Math.min(centerX, centerY);
+		
+		function drawArm(position, lengthScale, weight) {
+			pjs.strokeWeight(weight);
+			pjs.stroke(0,0,255);
+			pjs.line(centerX, centerY,
+							centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
+							centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);
+		}
+		
+		// erase background
+		pjs.background(0);
+		/*
+		var now = new Date();
+		
+		// Moving hours arm by small increments
+		var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;
+		drawArm(hoursPosition, 0.5, 5);
+		
+		// Moving minutes arm by small increments
+		var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;
+		drawArm(minutesPosition, 0.80, 3);
+		
+		// Moving hour arm by second increments
+		var secondsPosition = now.getSeconds() / 60;
+		drawArm(secondsPosition, 0.90, 1);
+		*/
+		pjs.fill(204, 102, 0);
+		pjs.rect(pjs.mouseX, pjs.mouseY, 10, 10);
 
-	walker.step();
-	
-	//rect.translation.set(two.width/2, two.height/2);
-
-	//rect.translation.set(mouse.x, mouse.y);
-    //rect.rotation += 0.005;
-});
+		walker.step();
+		walker.draw();
+	};
+}
+var canvasElem = document.getElementById("my-canvas");
+pjs = new Processing(canvasElem, sketchProc);
